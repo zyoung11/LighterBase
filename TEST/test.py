a062871edd9e74b86ca6ca977966f045ea4fcd27
lxt <2134024636@qq.com>
2025-10-09 15:11:57 +0800

    update test.py

--- TEST/test.py
+++ TEST/test.py
@@ -21,8 +21,38 @@ def create_user(name: str, password_hash: str, email: str) -> None:
     except requests.exceptions.RequestException as e:
         print("请求出错:", e)
 
-# 示例调用
-if __name__ == "__main__":
-    create_user("alice",
+create_user("alice",
                 "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8",
                 "alice@example.com")
+
+create_user("zellij",
+                "今天天气真好，适合出去走走。",
+                "163@@.com")
+
+
+
+
+def login_user(name: str, password_hash: str) -> None:
+    url = "http://localhost:8080/api/auth/login"
+    payload = {
+        "name": name,
+        "password_hash": password_hash
+    }
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    try:
+        response = requests.post(url, data=json.dumps(payload), headers=headers)
+
+        print("Status Code:", response.status_code)
+        print("Response Body:", response.text)
+    except requests.exceptions.RequestException as e:
+        print("请求出错:", e)
+
+login_user("alice",
+                "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8")
+
+login_user("zellij",
+                "今天天气真好，适合出去走走。")
